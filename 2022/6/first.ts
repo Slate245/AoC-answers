27c594621ef81f44478eb58de5464d0e7bb78187
Ivan Bespalov <slate245@gmail.com>
2022-12-06 10:56:55 +0500

    Day 6

---
+++ 2022/6/first.ts
@@ -0,0 +1,28 @@
+import { readLines } from "https://deno.land/std@0.167.0/io/mod.ts";
+
+const lines = readLines(await Deno.open("input.txt"));
+
+const input = Array.from(String((await lines.next()).value));
+
+const checkForUniq = (string: string[]) => {
+  let result = true;
+  for (let index = 0; index < string.length - 1; index++) {
+    const char = string[index];
+    const rest = string.slice(index + 1);
+    if (Array.from(rest).some((c) => c === char)) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+};
+
+let result = -1;
+for (let index = 3; index < input.length; index++) {
+  const isBufferUnique = checkForUniq(input.slice(index - 3, index + 1));
+  if (isBufferUnique) {
+    result = index + 1;
+    break;
+  }
+}
+console.log(result);
