f303495babd6c8f78815685788b28c34830473f2
Ivan Bespalov <slate245@gmail.com>
2024-12-09 18:15:11 +0500

    2024 Day 8 part 2

--- 2024/8/main.go
+++ 2024/8/main.go
@@ -34,7 +34,7 @@ func firstPart() {
 	for _, towers := range freqToTowersMap {
 		towerIdxPairs := getIndexPairs(len(towers))
 		for _, idxs := range towerIdxPairs {
-			antinodes := getAntinodes(towers[idxs[0]], towers[idxs[1]], bounds)
+			antinodes := getAntinodes(towers[idxs[0]], towers[idxs[1]], bounds, 1)
 
 			for _, an := range antinodes {
 				key := fmt.Sprint(an)
@@ -49,6 +49,24 @@ func firstPart() {
 
 func secondPart() {
 	slog.Debug("Running second part")
+
+	freqToTowersMap, bounds := prepareInput(input)
+
+	result := make(map[string]bool)
+
+	for _, towers := range freqToTowersMap {
+		towerIdxPairs := getIndexPairs(len(towers))
+		for _, idxs := range towerIdxPairs {
+			antinodes := getAntinodes(towers[idxs[0]], towers[idxs[1]], bounds, -1)
+
+			for _, an := range antinodes {
+				key := fmt.Sprint(an)
+				result[key] = true
+			}
+		}
+	}
+
+	fmt.Println(len(result))
 }
 
 func prepareInput(input string) (map[string][][2]int, [2]int) {
@@ -80,18 +98,42 @@ func getIndexPairs(len int) [][2]int {
 	return result
 }
 
-func getAntinodes(t1 [2]int, t2 [2]int, bounds [2]int) [][2]int {
+func getAntinodes(t1 [2]int, t2 [2]int, bounds [2]int, depth int) [][2]int {
 	delta := [2]int{t1[0] - t2[0], t1[1] - t2[1]}
 	var antinodes [][2]int
-	an1 := [2]int{t1[0] + delta[0], t1[1] + delta[1]}
-	an2 := [2]int{t2[0] - delta[0], t2[1] - delta[1]}
-
-	if an1[0] < bounds[0] && an1[0] >= 0 && an1[1] < bounds[1] && an1[1] >= 0 {
-		antinodes = append(antinodes, an1)
+	if depth < 0 {
+		antinodes = append(antinodes, t1, t2)
 	}
-
-	if an2[0] < bounds[0] && an2[0] >= 0 && an2[1] < bounds[1] && an2[1] >= 0 {
+	var vt1 [2]int = t1
+	var vt2 [2]int = t2
+	i := 0
+	for {
+		if depth > 0 && i == depth {
+			i = 0
+			break
+		}
+		an1 := [2]int{vt1[0] + delta[0], vt1[1] + delta[1]}
+		if an1[0] > bounds[0]-1 || an1[0] < 0 || an1[1] > bounds[1]-1 || an1[1] < 0 {
+			i = 0
+			break
+		}
+		antinodes = append(antinodes, an1)
+		vt1 = an1
+		i++
+	}
+	for {
+		if depth > 0 && i == depth {
+			i = 0
+			break
+		}
+		an2 := [2]int{vt2[0] - delta[0], vt2[1] - delta[1]}
+		if an2[0] > bounds[0]-1 || an2[0] < 0 || an2[1] > bounds[1]-1 || an2[1] < 0 {
+			i = 0
+			break
+		}
 		antinodes = append(antinodes, an2)
+		vt2 = an2
+		i++
 	}
 
 	return antinodes
